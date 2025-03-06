6c66e9fabd35a1137589948c9e43b732bdb37d95
jhs512 <jangka512@gmail.com>
2025-03-06 10:21:29 +0900

    work

---
+++ src/main/kotlin/com/back/domain/home/home/controller/HomeController.kt
@@ -0,0 +1,20 @@
+package com.back.domain.home.home.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.net.InetAddress
+
+
+@Controller
+class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    fun main(): String {
+        val localHost = InetAddress.getLocalHost()
+
+        val hostname = localHost.hostName
+
+        return "main, hostname : $hostname"
+    }
+}
