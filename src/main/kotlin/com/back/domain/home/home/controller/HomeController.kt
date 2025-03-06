1a8904e28653c80ce8dbbae1f9d1701731998730
jhs512 <jangka512@gmail.com>
2025-03-06 11:31:08 +0900

    work

--- src/main/kotlin/com/back/domain/home/home/controller/HomeController.kt
+++ src/main/kotlin/com/back/domain/home/home/controller/HomeController.kt
@@ -1,5 +1,6 @@
 package com.back.domain.home.home.controller
 
+import com.back.standard.extensions.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -8,12 +9,17 @@ import java.net.InetAddress
 
 @Controller
 class HomeController {
+    private val log = logger()
+
     @GetMapping("/")
     @ResponseBody
     fun main(): String {
         val localHost = InetAddress.getLocalHost()
 
         val hostname = localHost.hostName
+
+        println("모든 환경에서 출력")
+        log.debug("개발/테스트 환경에서만 출력")
 
         return "main(execution : 2nd), hostname : $hostname"
     }
