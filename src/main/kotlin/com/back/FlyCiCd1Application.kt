a0a421a8133f05e00eb4c29f50be12504fafbcb9
jhs512 <jangka512@gmail.com>
2025-03-06 10:05:13 +0900

    work

---
+++ src/main/kotlin/com/back/FlyCiCd1Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlyCiCd1Application
+
+fun main(args: Array<String>) {
+    runApplication<FlyCiCd1Application>(*args)
+}
